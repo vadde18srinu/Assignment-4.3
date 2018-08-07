a7110fa7c43aab41f21103328a513c729d68e0b8
vadde18srinu <40682358+vadde18srinu@users.noreply.github.com>
2018-08-07 13:16:00 +0530

    Add files via upload

---
+++ Assignment4_3.R
@@ -0,0 +1,53 @@
+
+1. States = rownames(US Arrests)
+Get states names with 'w'.
+Get states names with 'W'.
+
+library(states)
+class(USArrests)
+USArrests
+States=rownames(USArrests,do.NULL = TRUE, prefix = "row")
+States
+
+df=data.frame(States)
+df
+
+for (i in 47:50) {print(States[i])}
+
+#Which are 4 states starts with "W"  
+Washington 
+West Virginia.
+Wisconsin.
+Wyoming.
+
+states = c("Washington", "West Virginia", "Wisconsin", "Wyoming") 
+states
+
+states = toupper("Washington, West Virgina, Wisconsin, Wyoming")
+print(states)
+
+states = tolower("Washington, West Virgina, Wisconsin, Wyoming")
+print(states)
+
+
+2. Prepare a Histogram of the number of characters in each US state.
+string within the vector to form a final single string and assigning it the object.
+
+library(ggplot2)
+library(states)
+State=data.frame(states)
+State
+
+nchar(States, type = "bytes")
+
+hist(c(nchar(States, type = "bytes")),
+     main="Histogram of number of characters in each US state", 
+     xlab= "States",
+     border="blue", 
+     col="yellow",
+     xlim=c(3,15),
+     ylim =c(0,14),
+     las=1,
+     breaks=25)
+
+
